dd1cdcb645d22d0be9d2421749102a84fa69f874
Antonio Grass <asgrass@uc.cl>
2012-03-06 15:58:15 -0300

    cambios en el css y en el formado de ap.js

--- iOS1.3/www/js/app.js
+++ iOS1.3/www/js/app.js
@@ -3,15 +3,10 @@ var host = "../";
 
 
 $(document).bind("mobileinit", function(){
-
 	$.mobile.pushStateEnabled = true;
-
 	$.mobile.allowCrossDomainPages = true;
-
 	$.support.touchOverflow = true;
-
-  $.mobile.touchOverflowEnabled = true;
-
+   $.mobile.touchOverflowEnabled = true;
 
 
 
@@ -21,31 +16,22 @@ $(document).bind("mobileinit", function(){
 
 
 		$(function(){
-
 			var menuStatus;
 
 
 
 			// Show menu
 
-			$("a.showMenu").live("click", function (event) {
-			
+			$("a.showMenu").live("click", function (event) {			
 			  document.getElementById("bplayer").style.left = "-2000px";
-
 				if(menuStatus != true){	
-
 				document.getElementById("menu").style.visibility = 'visible';	
-
 				$(".ui-page-active").animate({
-
 					marginLeft: '165px'
 
 				  }, 300, function(){
-
 					  menuStatus = true;
-
 					   //$.mobile.loadPage( "ownLists.html" );
-
             			//$.mobile.loadPage( "friends.html" );
 
 					});
@@ -61,7 +47,6 @@ $(document).bind("mobileinit", function(){
 				  }, 300, function(){
 
 					  menuStatus = false;
-
 					  document.getElementById("menu").style.visibility = 'hidden';
 
 					});
@@ -86,21 +71,11 @@ $(document).bind("mobileinit", function(){
 
 
 				//$.mobile.loadPage(this.getAttribute("goTo")+".html");
-
 				$('.currentSeccion').removeClass('currentSeccion');
-
 				$(this).parent().addClass('currentSeccion');
-
-
-
 				$(".ui-page-active").css("margin-left","0");
-
 				menuStatus = false;
-
 				document.getElementById("menu").style.visibility = 'hidden';
-
-
-
 				return true;
 
 			});
@@ -126,15 +101,8 @@ $(document).bind("mobileinit", function(){
 
 
 			/*$("#ownLists .tabs li a, #favoriteLists .tabs li a").live("click", function (event) {
-
-
-
 				alert(this.getAttribute("tab"));
-
 				$.mobile.loadPage(this.getAttribute("tab")+".html");
-
-
-
 				return true;
 
 			});*/
@@ -144,25 +112,14 @@ $(document).bind("mobileinit", function(){
 
 
 			$( '#ownLists' ).live( 'pageshow',function(event, ui)
-
-			{
-
-				$.mobile.showPageLoadingMsg();
-
+			{	$.mobile.showPageLoadingMsg();
   				var list = $( "#ownLists" ).find( "#owns" );
-
 				list.empty();
-
 				var html = '<li><a href="acura.html">Acura</a></li> <li><a href="audi.html">Audi</a></li><li><a href="bmw.html">BMW</a></li><li><a href="acura.html">Acura</a></li> <li><a href="audi.html">Audi</a></li><li><a href="bmw.html">BMW</a></li><li><a href="acura.html">Acura</a></li> <li><a href="audi.html">Audi</a></li><li><a href="bmw.html">BMW</a></li><li><a href="acura.html">Acura</a></li> <li><a href="audi.html">Audi</a></li><li><a href="bmw.html">BMW</a></li><li><a href="acura.html">Acura</a></li> <li><a href="audi.html">Audi</a></li><li><a href="bmw.html">BMW</a></li>';
-
 				list.append(html);
-
 				$('#owns').listview("refresh");
-
 				//$.mobile.loadPage("favoriteLists.html");
-
 				//$.mobile.loadPage( "friends.html" );
-
 				$.mobile.hidePageLoadingMsg();
 
 			});
@@ -170,23 +127,13 @@ $(document).bind("mobileinit", function(){
 
 
 			$( '#favoriteLists' ).live( 'pageshow',function(event, ui)
-
-			{
-
-				$.mobile.showPageLoadingMsg();
-
+			{	$.mobile.showPageLoadingMsg();
   				var list = $( "#favoriteLists" ).find( "#favorites" );
-
 				list.empty();
-
 				var html = '<li><a href="acura.html">Acura</a></li> <li><a href="audi.html">Audi</a></li><li><a href="bmw.html">BMW</a></li>';
-
 				list.append(html);
-
 				$('#favorites').listview("refresh");
-
 				//$.mobile.loadPage("ownLists.html");
-
 				$.mobile.hidePageLoadingMsg();
 
 			});
@@ -196,37 +143,24 @@ $(document).bind("mobileinit", function(){
 
 
 			$("#searchMusic #musicForm").submit(function() {
-
   				var search = this.elements[0].value;
-
   				if(search == 0)
-
   				return false;
-
   				$.mobile.showPageLoadingMsg();	
-
-  					var dir = host + "ayax/busqueda2.php?q="+search;
-
+  				var dir = host + "ayax/busqueda2.php?q="+search;
   					//alert(dir);
 
 					var request = $.ajax({
-
       					type: "GET",
-
       					url: dir,
-
 	  					cache: false,
 
      					});
 
      				request.done(function(msg) {
-
   							var list = $( "#searchMusic" ).find( "#music" );
-
 							list.empty();
-
 							list.append(msg);
-
 							$('#music').listview("refresh");
 
 						});
@@ -234,13 +168,10 @@ $(document).bind("mobileinit", function(){
 
 
 					request.fail(function(jqXHR, textStatus) {
-
   							alert( "Request failed: " + textStatus );
 
 						});
-
 				$.mobile.hidePageLoadingMsg();
-
     			return false;
 
 			});
@@ -250,45 +181,28 @@ $(document).bind("mobileinit", function(){
 			$("#searchLists #searchForm").submit(function() {
 
   				var search = this.elements[0].value;
-
   				if(search == 0)
-
   				return false;
-
   				$.mobile.showPageLoadingMsg();	
-
   					var dir = host + "ayax/busqueda2.php?q="+search;
-
   					//alert(dir);
-
 					var request = $.ajax({
-
       					type: "GET",
-
       					url: dir,
-
 	  					cache: false,
-
      					});
 
      				request.done(function(msg) {
-
   							var list = $( "#searchLists" ).find( "#lists" );
-
 							list.empty();
-
 							list.append(msg);
-
 							$('#lists').listview("refresh");
-
 						});
 
 
 
 					request.fail(function(jqXHR, textStatus) {
-
   							alert( "Request failed: " + textStatus );
-
 						});
 
 				$.mobile.hidePageLoadingMsg();
@@ -302,27 +216,15 @@ $(document).bind("mobileinit", function(){
 
 
 			$( '#friends' ).live( 'pageshow',function(event, ui)
-
 			{
-
 				//alert(this.getAttribute("status"));
-
 				if(this.getAttribute("status"))
-
 				{
-
 				$.mobile.showPageLoadingMsg();
-
   				getFriends();
-
-
-
   				this.removeAttribute("status")
-
 				//$.mobile.loadPage("ownLists.html");
-
 				//$.mobile.hidePageLoadingMsg();
-
 			}
 
 			//alert(this.getAttribute("status"));
