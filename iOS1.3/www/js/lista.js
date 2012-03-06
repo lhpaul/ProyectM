36e0e36f2082cb60cfd13588892b7355091292d2
Antonio Grass <asgrass@uc.cl>
2012-03-06 16:56:36 -0300

    nuevo js con los metodos de las listas

---
+++ iOS1.3/www/js/lista.js
@@ -0,0 +1,26 @@
+//funcion que se carga al apretar el la secion #myownlist
+function onLoadMyList()
+{
+
+var id = 1;
+
+//Cambiar Nombre menu
+
+     var list = $( "#menu" ).find( "#memberDetails" );
+                list.empty();
+                var name = GetFacebookName();
+                var html = '<h3><span>'+name+'</span></h3>';
+                list.append(html);
+
+                var list = $( "#menu" ).find( "#menuList" );
+                list.empty();
+                var html = '<li><a href="#ownLists"class="ui-btn ui-btn-up-a ui-btn-corner-all ui-btn-icon-right ui-shadow ui-btn-up-undefined"><span class="ui-btn-inner ui-btn-corner-all ui-corner-top ui-corner-bottom"><span>My Lists</span><span class="ui-icon ui-icon-star ui-icon-shadow"></span></span></a></li><li><a href="#searchMusic"class="ui-btn ui-btn-up-a ui-btn-corner-all ui-btn-icon-right ui-shadow ui-btn-up-undefined"><span class="ui-btn-inner ui-btn-corner-all ui-corner-top ui-corner-bottom"><span>Search</span><span class="ui-icon ui-icon-star ui-icon-shadow"></span></span></a></li><li><a href="#friends"class="ui-btn ui-btn-up-a ui-btn-corner-all ui-btn-icon-right ui-shadow ui-btn-up-undefined"><span class="ui-btn-inner ui-btn-corner-all ui-corner-top ui-corner-bottom"><span>Friends</span><span class="ui-icon ui-icon-star ui-icon-shadow"></span></span></a></li><li><a href="#searchMusic"class="ui-btn ui-btn-up-a ui-btn-corner-all ui-btn-icon-right ui-shadow ui-btn-up-undefined"><span class="ui-btn-inner ui-btn-corner-all ui-corner-top ui-corner-bottom"><span>Logout</span><span class="ui-icon ui-icon-star ui-icon-shadow"></span></span></a></li>';
+                list.append(html);
+
+
+
+
+
+}
+
+
